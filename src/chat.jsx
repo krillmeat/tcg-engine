85dceddc9256ec924855e471a021127479f0e888
Ross <conover.ross@gmail.com>
2021-07-30 23:27:35 -0500

    making that baby more secure

--- src/chat.jsx
+++ src/chat.jsx
@@ -2,7 +2,7 @@ import React,{useEffect, useState} from 'react';
 import ChatInput from './chat-input';
 import ChatMessage from './chat-message';
 
-const URL = 'ws://tcg-engine.herokuapp.com';
+const URL = 'wss://tcg-engine.herokuapp.com';
 
 const Chat = () => {
 
