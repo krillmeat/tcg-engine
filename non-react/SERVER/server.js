c31b5f298910df51eb303e9d09b95c4b66e49660
Ross <conover.ross@gmail.com>
2021-08-06 01:53:30 -0500

    send a non-string?

--- non-react/SERVER/server.js
+++ non-react/SERVER/server.js
@@ -7,10 +7,10 @@ let ws = new WebSocket(URL);
     ws.onopen = () => {
       let id = username; // Todo - Make this attach to username from login
       console.log("WS - Open = "+username);
-      ws.send(JSON.stringify({
+      ws.send({
         actnName: 'join',
         actionTarget: id
-      }));
+      });
     }
 
     ws.onmessage = message => {
