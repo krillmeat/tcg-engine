478f685f1f9d122bfe1ae795301d3b5d8158db8d
Ross <conover.ross@gmail.com>
2021-08-06 01:59:17 -0500

    trying to stringify again

--- non-react/SERVER/server.js
+++ non-react/SERVER/server.js
@@ -7,10 +7,10 @@ let ws = new WebSocket(URL);
     ws.onopen = () => {
       let id = username; // Todo - Make this attach to username from login
       console.log("WS - Open = "+username);
-      ws.send({
+      ws.send(JSON.stringify({
         actnName: 'join',
         actionTarget: id
-      });
+      }));
     }
 
     ws.onmessage = message => {
