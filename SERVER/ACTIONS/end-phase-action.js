d04a3668333ae0b0aa4ea29f1b612cc52717584e
Ross <conover.ross@gmail.com>
2021-08-24 10:02:35 -0400

    removed the React component

--- SERVER/ACTIONS/end-phase-action.js
+++ SERVER/ACTIONS/end-phase-action.js
@@ -1,30 +1,43 @@
 const { sendAll, getOpponentNumber } = require("../utils.js");
 const setupAction = require("../ACTIONS/setup-action.js");
 
+/**----------------------------------------------------------------------------------
+ * END PHASE ACTION
+ * ----------------------------------------------------------------------------------
+ * Action for Ending the Current Phase
+ * Handles the Logic for setting up the next Phase
+ * ----------------------------------------------------------------------------------
+ * @param {Object}    action  Message from the Client 
+ * @param {Object}    LOBBY   LOBBY Object
+ * @param {WebSocket} ws      WebSocket Connection for Client
+ *----------------------------------------------------------------------------------*/
 const endPhaseAction = (action, LOBBY, ws) => {
     let phase = action.actnValue;
     let player = action.actnPlayer;
     let mockAction = {};
-    console.log("Ending Phase ",phase);
+    
     switch(phase){
-        case 'setup':
-            if(player === LOBBY.GAME_STATE.firstPlayer){ // If the first player is ending their setup
-                LOBBY.GAME_STATE.currentPlayer = getOpponentNumber(player);
-                mockAction = {actnName: 'going-first', actnPlayer: LOBBY.GAME_STATE.currentPlayer, actnRunner: 'server'};
-                setupAction(mockAction, LOBBY, ws);
-            } else{ // Second player is ending their setup
-                LOBBY.GAME_STATE.turn = 1;
-                LOBBY.GAME_STATE.currentPlayer = getOpponentNumber(player);
-                LOBBY.GAME_STATE.currentPhase = 'unsuspend';
-                mockAction = {actnName: 'unsuspend-phase', actnPlayer: LOBBY.GAME_STATE.currentPlayer, actnRunner: 'server'};
-                unsuspendPhaseAction(mockAction, LOBBY, ws);
-            }
-            break;
-        case 'unsuspend':
-            LOBBY.GAME_STATE.currentPhase = 'breeding';
-            mockAction = {actnName: 'breeding-phase', actnPlayer: player, actnRunner: 'server'};
-            breedingPhaseAction(mockAction, LOBBY, ws);
-            break;
+      case 'setup':
+        if(player === LOBBY.GAME_STATE.firstPlayer){ // If the first player is ending their setup
+            LOBBY.GAME_STATE.currentPlayer = getOpponentNumber(player);
+            mockAction = {actnName: 'going-first', actnPlayer: LOBBY.GAME_STATE.currentPlayer, actnRunner: 'server'};
+            setupAction(mockAction, LOBBY, ws);
+        } else{ // Second player is ending their setup
+            LOBBY.GAME_STATE.turn = 1;
+            LOBBY.GAME_STATE.currentPlayer = getOpponentNumber(player);
+            LOBBY.GAME_STATE.currentPhase = 'unsuspend';
+            mockAction = {actnName: 'unsuspend-phase', actnPlayer: LOBBY.GAME_STATE.currentPlayer, actnRunner: 'server'};
+            unsuspendPhaseAction(mockAction, LOBBY, ws);
+        }
+        break;
+      case 'unsuspend':
+        LOBBY.GAME_STATE.currentPhase = 'breeding';
+        mockAction = {actnName: 'breeding-phase', actnPlayer: player, actnRunner: 'server'};
+        breedingPhaseAction(mockAction, LOBBY, ws);
+        break;
+      default:
+        console.log("End Phase Action not Found...");
+        break;
     }
 }
 
--- SERVER/utils.js
+++ SERVER/utils.js
@@ -2,6 +2,14 @@ function logServer(message,object){
   object ? console.log(`%c ${message}%o`,'color: #FD971F',object) : console.log(`%c ${message}`,'color: #FD971F');
 }
 
+/**----------------------------------------------------------------------------------
+ * SEND ALL
+ * ----------------------------------------------------------------------------------
+ * Send a message from the Server to all of the Clients in a Lobby
+ * ----------------------------------------------------------------------------------
+ * @param {Object} lobby    LOBBY object
+ * @param {Object} message  Message for the Client
+ *----------------------------------------------------------------------------------*/
 function sendAll(lobby,message){
   for(var i=0; i<lobby.CLIENTS.length;i++){
     lobby.CLIENTS[i].send(message);
